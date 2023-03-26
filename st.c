849cc76bc0fa25360edde02859927514006235b6
yahorni <neuralorge@gmail.com>
2023-03-27 01:54:54 +0300

    patch: glyph wide support

--- st.c
+++ st.c
@@ -2823,9 +2823,11 @@ draw(void)
 	drawregion(0, 0, term.col, term.row);
 	if (term.scr == 0)
 		xdrawcursor(cx, term.c.y, TLINE(term.c.y)[cx],
-				term.ocx, term.ocy, TLINE(term.ocy)[term.ocx]);
+				term.ocx, term.ocy, TLINE(term.ocy)[term.ocx],
+				TLINE(term.ocy), term.col);
 		// xdrawcursor(cx, term.c.y, term.line[term.c.y][cx],
 				// term.ocx, term.ocy, term.line[term.ocy][term.ocx]);
+				// term.line[term.ocy], term.col);
 	term.ocx = cx;
 	term.ocy = term.c.y;
 	xfinishdraw();
